096c5b88c52cfd18c8f478f16a6df434885d2c5a
Usman Malik <usmanmalikk2004@gmail.com>
2024-05-06 16:27:30 +0500

    created app.js file and modified index file

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SplashScreen from "./pages/splashScreen";
+import dashboard from "./pages/dashboard";
+const App = () => {
+    return (
+         <SplashScreen />
+    );
+};
+export default App;
